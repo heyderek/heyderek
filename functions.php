86e259169714e821becab6547d5bb2bf5a080427
Derek <heyderek@dereknelson.net>
2013-01-05 13:29:43 -0800

    Added blog support.

--- functions.php
+++ functions.php
@@ -6,6 +6,14 @@ add_theme_support('post-thumbnails');
 //Add image sizes
 add_image_size('project-thumbnail', 605, 605, true );
 
+//Change the excerpt and make it pretty.
+function readmore_excerpt($more) {
+  global $post;
+  return '<a class="excerpt-link" href="' .get_permalink($post-ID) . '">Read Some More</a>';
+}
+
+add_filter('excerpt_more', 'readmore_excerpt');
+
 //Call in Gallery Post Type
 require_once('admin/gallery.php');
 
