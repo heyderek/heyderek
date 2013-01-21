472115892d196d70e2f3a1cbac687b9f24151d09
Derek <heyderek@dereknelson.net>
2013-01-21 11:43:58 -0800

    Modified the footer to include analytics trakcing.

--- footer.php
+++ footer.php
@@ -4,6 +4,17 @@
           <h6>&copy; 2013 Derek Nelson Creative LLC.  All Rights Reserved.</h6>
         </footer><!-- /.wrapper -->
         <?php wp_footer(); ?>
+        <script type="text/javascript">
+          var _gaq = _gaq || [];
+          _gaq.push(['_setAccount', 'UA-25708409-1']);
+          _gaq.push(['_trackPageview']);
+        
+          (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+          })();
+        </script>
       </footer><!-- /.colophon -->
     </div><!-- /.page -->
   </body>
