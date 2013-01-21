5e3947f369ff561af36eb1702b1947550b898e91
Derek <heyderek@dereknelson.net>
2013-01-21 11:16:56 -0800

    Added a bunch of stuff to the hmtl folder.

---
+++ html/js/contact-form.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+    if ($('body.home')
+        .length) {
+        // Validate the contact form
+        $('#contact-form').validate({
+            // Add requirements to each of the fields
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                message: {
+                    required: true,
+                    minlength: 10
+                }
+            },
+            // Specify what error messages to display
+            // when the user does something horrid
+            messages: {
+                name: {
+                    required: "Please enter your name.",
+                    minlength: jQuery.format("At least {0} characters required.")
+                },
+                email: {
+                    required: "Please enter your email.",
+                    email: "Please enter a valid email."
+                },
+                message: {
+                    required: "Please enter a message.",
+                    minlength: jQuery.format("At least {0} characters required.")
+                }
+            },
+            // Use Ajax to send everything to /php/form.php
+            submitHandler: function (form) {
+                $("#submit").attr("value", "Sending...");
+                $(form).ajaxSubmit({
+                    target: "#response",
+                    success: function (responseText, statusText, xhr, $form) {
+                        $('#contact_container').slideUp("fast");
+                        $("#response").html(responseText).hide().slideDown("fast");
+                    }
+                });
+                return false;
+            }
+        });
+    };
+});
---
+++ html/js/custom.js
@@ -0,0 +1,83 @@
+$(document).ready(function(){
+
+  //Add Classes for CSS3 Animations
+  $('.logo, .copy').addClass('ready');
+
+  //Scrollto functionality for navigation
+  $('a#work').click(function(){
+    $.scrollTo('.work', {'duration':'fast'});
+    return false;
+  });
+  $('a#about').click(function(){
+    $.scrollTo('.about', {'duration':'fast'});
+    return false;
+  });
+  $('a#contact').click(function(){
+    $.scrollTo('.contact', {'duration':'fast'});
+    return false;
+  });
+  $('button#chat').click(function(){
+    $.scrollTo('.contact', {'duration':'fast'});
+    return false;
+  });
+  $('button.tothetop').click(function(){
+    $.scrollTo('.page', {'duration':'fast'});
+    return false;
+  });
+
+  //Mobile navigation button
+  $('.shownav').click(function(){
+    if ($(this).hasClass('open')){
+      $(this).removeClass('open');
+      $('.access').slideUp();
+    } else {
+      $(this).addClass('open');
+      $('.access').slideDown();
+    }
+  });
+  //Fix mobile nav on browser resize
+  $(window).resize(function(){
+    var docWidth = $(window).width();
+
+    if(docWidth > 539){
+        $('.access').show();
+      } else {
+        $('.access').hide();
+      }
+  });
+
+ //Gallery
+   var briefHeight;
+   
+   $('.brief:first').show();
+   setTimeout(function(){
+     briefHeight = $('.brief:first').innerHeight();
+     $('.gallery > li:first-child').find('.frame').addClass('active');
+     $('.gallery > li:first-child').children('.brief').addClass('open').show();
+     $('.gallery > li:first-child').css('marginBottom', briefHeight);
+   }, 100)
+
+
+    briefHeight = $('.brief:first').innerHeight();
+    $('.gallery > li:first-child').css('marginBottom', briefHeight);
+
+    //console.log('docwidth :', docWidth);
+
+  $('.gallery .frame > img').click(function(){
+    
+    var hideBrief = $('.brief').hide();
+    var showBrief = $(this).parent().next('article');
+    var briefLength = showBrief.innerHeight();
+    
+    $('.brief').removeClass('open');
+    showBrief.addClass('open');
+    $('.frame').removeClass('active');
+    $(this).parent().addClass('active');
+    $('.brief').parent().css('marginBottom', '0');
+    $(this).parents('.gallery > li').css('marginBottom', briefLength);
+
+    hideBrief;
+    showBrief.slideDown();
+  });
+  
+});
